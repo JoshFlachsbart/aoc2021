5c173262199d319d3e2dc7438384a0bb68fdc461
Josh Flachsbart <josh.flachsbart@climate.com>
2021-12-12 20:53:31 -0600

    Day 10 part 2.

--- src/com/bayer/aoc2021/day10/NavSyntax.java
+++ src/com/bayer/aoc2021/day10/NavSyntax.java
@@ -5,9 +5,11 @@ import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.Stack;
+import java.util.stream.Collectors;
 
 public class NavSyntax {
     public static Optional<Character> findError(String commandLine) {
@@ -38,6 +40,26 @@ public class NavSyntax {
         return Optional.empty();
     }
 
+    public static List<Character> autoComplete(String commandLine) {
+        Stack<Character> commands = new Stack<>();
+        List<Character> completion = new ArrayList<>();
+        for (char c: commandLine.toCharArray()) {
+            switch (c) {
+                case '(', '{', '[', '<' -> commands.push(c);
+                case ')', '}', ']', '>' -> commands.pop();
+            }
+        }
+        while (!commands.empty()) {
+            switch (commands.pop()) {
+                case '(' -> completion.add(')');
+                case '{' -> completion.add('}');
+                case '[' -> completion.add(']');
+                case '<' -> completion.add('>');
+            }
+        }
+        return completion;
+    }
+
     public static long getErrorVal(char errorChar) {
         return switch (errorChar) {
             case ')' -> 3;
@@ -48,8 +70,23 @@ public class NavSyntax {
         };
     }
 
+    public static long getAutocompleteVal(List<Character> completeChars) {
+        long score = 0;
+        for (char c : completeChars) {
+            score *= 5;
+            score += switch (c) {
+                case ')' -> 1;
+                case '}' -> 3;
+                case ']' -> 2;
+                case '>' -> 4;
+                default -> throw new IllegalArgumentException("Unknown error char: " + c);
+            };
+        }
+        return score;
+    }
+
     private Path getLocalPath(String fileName) throws URISyntaxException, IOException {
-        URL file = getClass().getClassLoader().getResource("com/bayer/aoc2021/day08/" + fileName);
+        URL file = getClass().getClassLoader().getResource("com/bayer/aoc2021/day10/" + fileName);
         if (file == null) throw new IOException("Unable to locate file: " + fileName);
         return Path.of(file.toURI()).toAbsolutePath();
     }
@@ -65,5 +102,13 @@ public class NavSyntax {
                 .reduce(Long::sum).orElseThrow();
 
         System.out.println(errorSum);
+
+        List<Long> scores = Files.lines(dataPath)
+                .filter(line -> NavSyntax.findError(line).isEmpty())
+                .map(NavSyntax::autoComplete)
+                .map(NavSyntax::getAutocompleteVal)
+                .sorted()
+                .collect(Collectors.toList());
+        System.out.println(scores.get(scores.size() / 2));
     }
 }
--- src/com/bayer/aoc2021/day10/NavSyntaxTest.java
+++ src/com/bayer/aoc2021/day10/NavSyntaxTest.java
@@ -3,6 +3,10 @@ package com.bayer.aoc2021.day10;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class NavSyntaxTest {
@@ -13,4 +17,27 @@ class NavSyntaxTest {
         Assertions.assertEquals(')', NavSyntax.findError("({)").get());
         Assertions.assertEquals('>', NavSyntax.findError("(<(()<>{}>)").get());
     }
+
+    @Test
+    void checkAutocomplete() {
+        Assertions.assertEquals(toCharList("}}]])})]"), NavSyntax.autoComplete("[({(<(())[]>[[{[]{<()<>>"));
+        Assertions.assertEquals(toCharList(")}>]})"), NavSyntax.autoComplete("[(()[<>])]({[<{<<[]>>("));
+        Assertions.assertEquals(toCharList("}}>}>))))"), NavSyntax.autoComplete("(((({<>}<{<{<>}{[]{[]{}"));
+        Assertions.assertEquals(toCharList("]]}}]}]}>"), NavSyntax.autoComplete("{<[[]]>}<{[{[{[]{()[[[]"));
+        Assertions.assertEquals(toCharList("])}>"), NavSyntax.autoComplete("<{([{{}}[<[[[<>{}]]]>[]]"));
+    }
+
+    private List<Character> toCharList(String str) {
+        return str.chars().mapToObj(c -> (char)c).collect(Collectors.toList());
+    }
+
+    @Test
+    void checkAutocompleteScore() {
+        Assertions.assertEquals(288957, NavSyntax.getAutocompleteVal(toCharList("}}]])})]")));
+        Assertions.assertEquals(5566, NavSyntax.getAutocompleteVal(toCharList(")}>]})")));
+        Assertions.assertEquals(1480781, NavSyntax.getAutocompleteVal(toCharList("}}>}>))))")));
+        Assertions.assertEquals(995444, NavSyntax.getAutocompleteVal(toCharList("]]}}]}]}>")));
+        Assertions.assertEquals(294, NavSyntax.getAutocompleteVal(toCharList("])}>")));
+    }
+
 }
